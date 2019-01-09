95c3eac0911ee2b335d35b9be0dfca4bbc585b2a
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-09 13:54:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  def initialize (persons_name)
-    @name = persons_name
+  def initialize (dogs_name)
+    @name = dogs_name
   end
 
-  def name=(persons_name)
-    @name = persons_name
+  def name=(dogs_name)
+    @name = dogs_name
   end
 
   def name
